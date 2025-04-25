0fcb2a570b5c2f7fbdacedf4c0c97fc6ce23e16b
Max Daiber-Huppert <mdh@ipa.fhg.de>
2025-04-26 00:13:29 +0200

    add webmin

--- script/template/dev/seed/data/debs/install.sh
+++ script/template/dev/seed/data/debs/install.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
+ROOT=$1
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cd $SCRIPT_DIR
+function install_local_debs {
+    cd $SCRIPT_DIR
 
-dpkg -i ./*.deb
-apt-get update
-apt-get install -f -y
+    dpkg -i ./*.deb
+    apt-get update
+    apt-get install -f -y
 
-cd -
+    cd -
+}
+
+function install_webmin {
+    cd $ROOT/tmp
+    
+    curl -o webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh 
+    sh webmin-setup-repo.sh
+    apt-get install --yes --install-recommends webmin usermin
+
+    cd -
+}
+
+install_local_debs
+install_webmin
