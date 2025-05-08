8847a5b32db889869422de4ecc52a2a755bf8b5c
mdh <max.daiber-huppert@ipa.fraunhofer.de>
2025-05-08 19:07:13 +0200

    Refactor CreateSeedStepper class: encapsulate step rendering logic and improve configuration handling

--- script/gui/main.py
+++ script/gui/main.py
@@ -4,8 +4,6 @@ import crypt
 from nicegui import ui, run, events
 from loguru import logger
 
-from utils_ui.simple_table import SimpleTable
-
 from step_ui.step_identity import StepIdentity
 from step_ui.step_hardware import StepHardware
 from step_ui.step_connectivity import StepConnectivity
@@ -60,48 +58,74 @@ def save_config(config):
         yaml.dump(config, f)
     ui.notify('Configuration saved')
 
+class CreateSeedStepper:
+    """
+    CreateSeed class to handle the creation of the seed ISO.
+    """
+    def __init__(self, config = None):
+        if config:
+            self.config = config
+        else:
+            self.config = DEFAULT_CONFIG.copy()
+            
+        self._render()
 
-with ui.stepper().props('horizontal header-nav').classes('w-full') as stepper:
-    with ui.step('Identity').classes('w-full flex-grow justify-items-center') as identiy_step:
-        with ui.column().classes('w-full'):
-            step_identity = StepIdentity(config)
-            
-            with ui.stepper_navigation().classes('w-full justify-end'):
-                ui.button('Next', on_click=stepper.next)
-    with ui.step('Hardware').classes('w-full flex-grow justify-items-center') as hardware_step:
-        with ui.column().classes('w-full'):
-            
-            step_hardware = StepHardware(config)
-            
-            with ui.stepper_navigation().classes('w-full justify-end'):
-                ui.button('Next', on_click=stepper.next)
-                ui.button('Back', on_click=stepper.previous).props('flat')
-    with ui.step('Connectivity').classes('w-full flex-grow justify-items-center'):
-        with ui.column().classes('w-full'):
-            
-            step_connectivity = StepConnectivity(config)
-            
-            with ui.stepper_navigation().classes('w-full justify-end'):
-                ui.button('Next', on_click=stepper.next)
-                ui.button('Back', on_click=stepper.previous).props('flat')
-    with ui.step('Create Seed').classes('w-full flex-grow justify-items-center'):
-        with ui.column().classes('w-full'):
-            
-            step_create_seed = StepCreateSeed(config)
-            
-            with ui.stepper_navigation().classes('w-full justify-end'):
-                ui.button('Done', on_click=lambda: ui.notify('Yay!', type='positive'))
-                ui.button('Back', on_click=stepper.previous).props('flat')
+    def _render(self):
+        with ui.stepper().props('horizontal header-nav').classes('w-full') as stepper:
+            with ui.step('Identity').classes('w-full flex-grow justify-items-center') as identiy_step:
+                with ui.column().classes('w-full'):
+                    step_identity = StepIdentity(config)
+                    
+                    with ui.stepper_navigation().classes('w-full justify-end'):
+                        ui.button('Next', on_click=stepper.next)
+            with ui.step('Hardware').classes('w-full flex-grow justify-items-center') as hardware_step:
+                with ui.column().classes('w-full'):
+                    
+                    step_hardware = StepHardware(config)
+                    
+                    with ui.stepper_navigation().classes('w-full justify-end'):
+                        ui.button('Next', on_click=stepper.next)
+                        ui.button('Back', on_click=stepper.previous).props('flat')
+            with ui.step('Connectivity').classes('w-full flex-grow justify-items-center'):
+                with ui.column().classes('w-full'):
+                    
+                    step_connectivity = StepConnectivity(config)
+                    
+                    with ui.stepper_navigation().classes('w-full justify-end'):
+                        ui.button('Next', on_click=stepper.next)
+                        ui.button('Back', on_click=stepper.previous).props('flat')
+            with ui.step('Create Seed').classes('w-full flex-grow justify-items-center'):
+                with ui.column().classes('w-full'):
+                    
+                    step_create_seed = StepCreateSeed(config)
+                    
+                    with ui.stepper_navigation().classes('w-full justify-end'):
+                        ui.button('Done', on_click=lambda: ui.notify('Yay!', type='positive'))
+                        ui.button('Back', on_click=stepper.previous).props('flat')
 
-# Save button
-ui.button('Save Configuration', on_click=lambda _: (
-    step_identity.update_config(),
-    step_hardware.update_config(),
-    step_connectivity.update_config(),
-    step_create_seed.update_config(),
-    save_config(config)
-))
+        # Save button
+        ui.button('Save Configuration', on_click=lambda _: (
+            step_identity.update_config(),
+            step_hardware.update_config(),
+            step_connectivity.update_config(),
+            step_create_seed.update_config(),
+            save_config(config)
+        ))
+
+    def create_seed_iso(self):
+        # Placeholder for creating seed ISO
+        pass
+
+    def download_seed_iso(self):
+        # Placeholder for downloading seed ISO
+        pass
 
 
+if __name__ in {"__main__", "__mp_main__"}:
+    # Load the YAML configuration
+    config = get_config(YAML_PATH)
 
-ui.run(title='YAML Configurator', port=8080)
+    # Create the GUI
+    css = CreateSeedStepper(config)
+    
+    ui.run(title='Robot Mindset Linux', port=8080)
