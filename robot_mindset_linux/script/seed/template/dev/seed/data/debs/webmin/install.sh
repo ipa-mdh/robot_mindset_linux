a8ab6a60aa0f40a217f6927c19c335db0f082efd
Max Daiber-Huppert <max.daiber-huppert@ipa.fraunhofer.de>
2025-09-17 13:06:44 +0000

    fix: update ansible version and improve kernel role handling in playbook; enhance webmin installation script

--- robot_mindset_linux/script/seed/template/dev/seed/data/debs/webmin/install.sh
+++ robot_mindset_linux/script/seed/template/dev/seed/data/debs/webmin/install.sh
@@ -2,4 +2,5 @@
 
 curl -o /tmp/webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/2.510/webmin-setup-repo.sh
 sh /tmp/webmin-setup-repo.sh --stable --force
+apt update
 apt-get install --yes --install-recommends webmin usermin
